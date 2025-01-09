197d2b5e53ef74758218feb477073334157f5ab4
lorrainy <lorrainy_71@hotmail.com>
2025-01-08 21:39:52 -0300

    add arquivo de variaves

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    },
-    env: {
-      trello_api_key: 'sua_api_key',
-      trello_api_token: 'seu_api_token',
-      trello_board_name: 'NomeDoSeuBoard',
-      trello_api_url: 'https://api.trello.com/1',
-      trello_members_url: 'https://api.trello.com/1/members/me/boards',
-      trello_members_fields_url: 'https://api.trello.com/1/members/me/boards?fields=name,url',
-      trello_boards_url: 'https://api.trello.com/1/boards',
-      trello_cards_url: 'https://api.trello.com/1/cards'
     }
   },
 });
